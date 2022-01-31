a35c73c057f83e008af16263f93cc3c3bac5b752
GaryBrightPercival <61967855+GaryBrightPercival@users.noreply.github.com>
2022-01-31 14:17:17 +0800

    chat msg update

--- post.php
+++ post.php
@@ -1,9 +1,24 @@
 <?php
 session_start();
+
+$A_TEMPLATE = "<div class='A'><img src='img/boy.png'><p>[MSG]</p><span>[TIME]</span></div>"
+$B_TEMPLATE = "<div class='B'><img src='img/man.png'><p>[MSG]</p><span>[TIME]</span></div>"
+
+
 if(isset($_SESSION['name'])){
     $text = $_POST['text'];
 	
-	$text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
+	if ($_SESSION['name'] == 'A'){
+		$text_message = str_replace("[MSG]", stripslashes(htmlspecialchars($text)), $A_TEMPLATE);
+		$text_message = str_replace("[TIME]", date("g:i A"), $text_message);
+	}
+	if ($_SESSION['name'] == 'B'){
+		$text_message = str_replace("[MSG]", stripslashes(htmlspecialchars($text)), $B_TEMPLATE);
+		$text_message = str_replace("[TIME]", date("g:i A"), $text_message);
+	}
+	
+	//$text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
+	
     file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
 }
 ?>
