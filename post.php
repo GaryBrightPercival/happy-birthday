9741994f682f29d7122f51fdb12f5206b328d71e
GaryBrightPercival <61967855+GaryBrightPercival@users.noreply.github.com>
2022-01-31 15:46:05 +0800

    test

--- post.php
+++ post.php
@@ -4,8 +4,8 @@ session_start();
 $A_TEMPLATE = "<div class='A'><img src='img/boy.png'><p>[MSG]</p><span>[TIME]</span></div>";
 $B_TEMPLATE = "<div class='B'><img src='img/man.png'><p>[MSG]</p><span>[TIME]</span></div>";
 
-$eightHours = DateInterval::createFromDateString('8 hours');
-$msgdate = date_add(date(),$eightHours);
+//$eightHours = DateInterval::createFromDateString('8 hours');
+//$msgdate = date_add(date(),$eightHours);
 
 //$msgdate = date_add($msgdate,$eightHours);
 
@@ -14,11 +14,11 @@ if(isset($_SESSION['name'])){
 	
 	if ($_SESSION['name'] == 'A'){
 		$text_message = str_replace("[MSG]", stripslashes(htmlspecialchars($text)), $A_TEMPLATE);
-		$text_message = str_replace("[TIME]", date_format($msgdate,"g:i A"), $text_message);
+		$text_message = str_replace("[TIME]", date(), $text_message);
 	}
 	if ($_SESSION['name'] == 'B'){
 		$text_message = str_replace("[MSG]", stripslashes(htmlspecialchars($text)), $B_TEMPLATE);
-		$text_message = str_replace("[TIME]", date_format($msgdate,"g:i A"), $text_message);
+		$text_message = str_replace("[TIME]", date(), $text_message);
 	}
 	
 	//$text_message = "<div class='msgln'><span class='chat-time'>".date("g:i A")."</span> <b class='user-name'>".$_SESSION['name']."</b> ".stripslashes(htmlspecialchars($text))."<br></div>";
