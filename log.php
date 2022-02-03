99751c217a47a1ad7c65b269d9f787333651a7e9
GaryBrightPercival <61967855+GaryBrightPercival@users.noreply.github.com>
2022-02-03 15:34:26 +0800

    show table

--- log.php
+++ log.php
@@ -1,10 +1,31 @@
 <?php
 require_once 'config.php';
 session_start();
-echo $connStr;
+//echo $connStr;
 
 $dbconn = pg_connect($connStr) or die("Could not connect");
 echo "Connected successfully";
+
+
+$query = 'SELECT * FROM chat_log ORDER BY id';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
+
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+
 pg_close($dbconn);
 
 ?>
