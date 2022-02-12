823781a6f6010bfe618660cb29e22823df7f646f
GaryBrightPercival <61967855+GaryBrightPercival@users.noreply.github.com>
2022-02-13 00:16:22 +0800

    clean up

--- post.php
+++ post.php
@@ -24,17 +24,5 @@ if(isset($_COOKIE[$COOKIE_NAME])){
 			$pdo = null;
 		}
 	}
-/*	
-	if ($_SESSION['name'] == 'A'){
-		$text_message = str_replace("[MSG]", stripslashes(htmlspecialchars($text)), $A_TEMPLATE);
-		$text_message = str_replace("[TIME]", date("g:i A"), $text_message);
-	}
-	if ($_SESSION['name'] == 'B'){
-		$text_message = str_replace("[MSG]", stripslashes(htmlspecialchars($text)), $B_TEMPLATE);
-		$text_message = str_replace("[TIME]", date("g:i A"), $text_message);
-	}
-
-    file_put_contents("log.html", $text_message, FILE_APPEND | LOCK_EX);
-*/
 }
 ?>
